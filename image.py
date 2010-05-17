1150e641bc7a174929f7c6a3781ca64bb8cdc823
Boryslav Larin <brabadu@gmail.com>
2010-05-18 00:17:55 +0300

    Effect 'put filter' was implemented in Image class

--- image.py
+++ image.py
@@ -5,6 +5,7 @@ import sys
 import re
 import os
 import random
+import operator
 
 from utils import InvalidImage, UnknownImageFormat
 
@@ -15,6 +16,13 @@ class Image ():
         self.height = 0
         self.width = 0
         self.load_modules()
+        self.f = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
+        self.filter_sum = reduce(operator.add, reduce(operator.add, self.f))
+
 
     def open(self, filepath):
         """Returns image object with a class of file type"""
@@ -53,10 +61,31 @@ class Image ():
         for line in self.bitmap:
             for pixel in line:
                 np = (pixel[c] + strength) % 65536
-                print pixel, " : ",
+#                print pixel, " : ",
                 pixel[c] = np > 0 and np or 0
-                print pixel
+#                print pixel
 
     def negative(self):
         self.bitmap = [[[65536 - component for component in pixel] for pixel in line] for line in self.bitmap]
+
+    def put_filter(self):
+        def multiply(x, y):
+            return ([x[i]*y for i in xrange(3)])
+
+        import copy
+        b = copy.deepcopy(self.bitmap)
+        for i,line in enumerate(self.bitmap[1:-1]):
+            for j,pixel in enumerate(line[1:-1]):
+                multies = (multiply(self.bitmap[i-1][j-1], self.f[0][0]),
+                        multiply(self.bitmap[i-1][j], self.f[0][1]),
+                        multiply(self.bitmap[i-1][j+1], self.f[0][2]),
+                        multiply(self.bitmap[i][j-1], self.f[1][0]),
+                        multiply(self.bitmap[i][j], self.f[1][1]),
+                        multiply(self.bitmap[i][j+1], self.f[1][2]),
+                        multiply(self.bitmap[i+1][j-1], self.f[2][0]),
+                        multiply(self.bitmap[i+1][j], self.f[2][0]),
+                        multiply(self.bitmap[i+1][j+1], self.f[2][0]))
+                center = reduce(lambda x,y: [x[k] + y[k] for k in xrange(3)], multies )
+                b[i][j] = [center[k] / self.filter_sum for k in xrange(3)]
+        self.bitmap = b
 
